0879fae08195b88adfd8ef1444027809f41274f9
rananorouzi <rn.norouzi@gmail.com>
2024-09-05 13:14:15 +0300

    Update TodoItem.cs
    Add Description field

--- TodoListApi/Models/TodoItem.cs
+++ TodoListApi/Models/TodoItem.cs
@@ -4,6 +4,7 @@ public class TodoItem
 {
     public long Id { get; set; }
     public required string Name { get; set; }
+    public string? Description { get; set; }
     public bool IsComplete { get; set; }
     public bool Pending {get; set; }
 }
